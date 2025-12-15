275aa3d9f451158d438ecc4500135952d3074db6
KanishkaCU <kanishkacu888@gmail.com>
2025-12-15 15:17:44 +0530

    Update project files

---
+++ src/com/training/day6/Problems.java
@@ -0,0 +1,76 @@
+package com.training.day6;
+
+public class Problems {
+
+	public static void main(String[] args) {
+//		int n = 3;
+//		int sum = 1;
+//		
+//		for (int i = 1;i<=n;i++)
+//			sum=sum*i;
+//			System.out.println(i+ " ");
+		
+//		Factorial sum
+//		int sum=0;
+//		
+//		int fact = 1;
+//		
+//		for(int j = 1 ; j <= 3; j++)
+//		{
+//			fact =1;
+//		
+//		for (int i = 1; i <= j; i++)
+//			fact=fact*i;
+//		
+//		sum = sum+fact;
+//		
+//		}
+//		System.out.println(sum);
+//		
+		
+		// prime or not
+//		int num  = 18; boolean temp = true;
+////		int flag = 0;
+//		
+//		for ( int i = 2 ; i < num; i++)
+//		{
+//			if(num % i == 0)
+//			{
+////				flag = 1;
+//				temp = false;
+//				break; 
+//			}
+//			
+//		}
+//		
+////		if (flag=0)
+//		
+//		if(temp)
+//			
+//			System.out.println("Prime");
+//		else
+//			System.out.println("Not Prime");
+//	
+//		
+//			
+//		
+		//pattern 1
+//		int n=5;
+//		for(int j=1 ; j<=n; j++)
+//		{
+//			for (int i = 1; i<=n; i++)
+//				System.out.print("*");
+//			System.out.println();
+//		}
+//		
+		int n =5;
+		for (int i = 1; i<=n; i++,System.out.println("hello"))
+			System.out.println("World");
+		
+		
+	}
+
+}
+
+
+
