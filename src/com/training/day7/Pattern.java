6aad674594634ec773d7f44a8c616f2e85716073
KanishkaCU <kanishkacu888@gmail.com>
2025-12-17 13:57:48 +0530

    push

---
+++ src/com/training/day7/Pattern.java
@@ -0,0 +1,78 @@
+package com.training.day7;
+
+public class Pattern {
+	public static void main(String[] args) {
+		
+		
+		//pattern 1
+//		int n = 5;
+//		for(int row=1 ; row<=n ; row++ , System.out.println())
+//		{
+//			for(int col=1 ; col<=n ; col ++)
+//				System.out.print("*");
+//		}
+//		
+//		
+		// pattern  2
+//		int n = 5;
+//		int r=3,c=6;
+//		for(int row=1 ; row<=r ; row++ , System.out.println())
+//		{
+//			for(int col=1 ; col<=c ; col ++)
+//				System.out.print("*");
+//		}
+//		
+
+//		
+//		int n = 5;
+//		for(int row=1 ; row<=n ; row++ , System.out.println())
+//		{
+//			for(int col=1 ; col<=row ; col ++)
+//				System.out.print("*");
+//		}
+		
+		//pattern 8
+//		int n = 5;
+//		for(int row=n ; row>=1 ; row-- , System.out.println())
+//		{
+//			for(int col=row ; col<=n ; col++)
+//				System.out.print(col);
+////
+////		
+//}
+		
+//		int n = 5;
+//		for(int row=1 ; row<=n ; row++ , System.out.println())
+//		{
+//			for(int col=n ; col>=n-row+1 ; col--)
+//				System.out.print(col);
+//		}
+		
+//		
+//		int n = 5;
+//		for(int row=1 ; row<=n ; row++ , System.out.println())
+//		{
+//			for(int col=row ; col>=1 ; col--)
+//				System.out.print((char)(col+64));
+//		}
+//		
+		
+		int n = 5;
+		for(int row=1 ; row<=n ; row++ , System.out.println())
+
+		{
+			
+			for(int space=1 ; space<row ; space++) System.out.print(" ");
+		
+
+			for(int col=row ; col<=n ; col++)
+				System.out.print("*");
+			
+			
+		}
+		
+		
+		
+		
+	}
+}
---
+++ src/com/training/day8/arra.java
@@ -0,0 +1,10 @@
+package com.training.day8;
+
+public class arra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
